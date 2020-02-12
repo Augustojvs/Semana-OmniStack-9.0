6cee2a4099dc72fd4e3f3d4bd0e67d5918f4ab74
CaVeIrA <augusto_jvs@hotmail.com>
2020-02-12 19:57:48 -0300

    first commit

---
+++ frontend/src/routes.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import New from './pages/New';
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Login} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/new" component={New} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
